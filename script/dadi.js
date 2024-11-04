6e0872c8d1da6701a599ec201b15f339f754fa95
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-11-04 15:18:55 +0100

    feat : completato

--- script/dadi.js
+++ script/dadi.js
@@ -1,8 +1,17 @@
 
-let usernumber = Math.ceil(Math.random() * 6);
-console.log(usernumber)
-let pcnumber = Math.ceil(Math.random() * 6);
-console.log(pcnumber)
+function lancioDado (){
+    let dado = Math.floor(Math.random() * 6) + 1;
+    return dado;
+}
+
+let usernumber = lancioDado();
+let pcnumber = lancioDado();
+console.log (usernumber, pcnumber)
+
+// let usernumber =  Math.ceil(Math.random() * 6);
+// console.log(usernumber)
+// let pcnumber = Math.ceil(Math.random() * 6);
+// console.log(pcnumber)
 
 if (usernumber > pcnumber) {
     console.log("complimenti hai vinto");
